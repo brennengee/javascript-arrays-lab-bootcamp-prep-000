cd55aafddb898f5529cd76ab4f801edfaa21fb36
Brennen Gee <brennengee@me.com>
2017-12-08 16:33:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var kittens = ["Milo", "Otis", "Garfield"]
 var name = "Ralph"
 
 function destructivelyAppendKitten(name) {
-  var appendKitten = appendKitten.slice()
+  var appendKitten = kittens.slice()
   appendKitten.push(name)
   return appendKitten
 }
