0c5557cb234fdf8bdda8410fc7a4ad8e5927fe59
Brennen Gee <brennengee@me.com>
2017-12-08 16:04:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var kittens = ["Tonka", "Peaches", "Darla", "Cat", "Whiskers", "Nietzsche"]
 var k = "Rat"
 
 function destructivelyAppendKitten(kittens, k) {
-  kittens.push(k)
+  (k)
   return kittens
 }
